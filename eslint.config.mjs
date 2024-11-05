7145d9ab807a82d78a3bfe359c2fe64363c5a6b5
mortolait <contatomortola@outlook.com>
2024-11-05 07:32:31 -0300

    chore: add eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,24 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tsEslintParser from "@typescript-eslint/parser";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    "env": {
-      "vitest-globals/env": true
-    }
+/** @type {import('eslint').Linter.Config} */
+export default {
+  files: ["**/*.{js,mjs,cjs,ts}"],
+  languageOptions: {
+    parser: tsEslintParser,
+    sourceType: "module",
+    globals: globals.node,
   },
-  {
-    "extends": [
-    "plugin:vitest-globals/recommended"
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
+  plugins: [
+    "@typescript-eslint"
+  ],
+  rules: {
+    "no-console": "warn",
+    "semi": ["error", "always"],
+    "quotes": ["error", "single"]
   }
-];
+};
